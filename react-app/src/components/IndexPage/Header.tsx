1fe9d013199f2b30eb358878b5c9da93e7b64b0b
AndrianHaliuk <vse1dobre@gmail.com>
2025-05-26 09:56:55 +0200

    fix

--- react-app/src/components/IndexPage/Header.tsx
+++ react-app/src/components/IndexPage/Header.tsx
@@ -4,15 +4,17 @@ const Header: React.FC = () => {
   return (
     <header role="banner">
       <div className="container">
-        <div className="title">
-          <h1>Інклюзивний догляд за дітьми з особливими потребами</h1>
-          <div className="title-buttons">
-            <a className="sec-btn" href="/what-we-do">Що ми робимо</a>
-            <div>
-              <svg className="playicon">
-                <use xlinkHref="/img/sprite.svg#Play icon"></use>
-              </svg>
-              <a className="video-btn" href="https://www.youtube.com/watch?v=rkRyetRMrjQ">Переглянути відео</a>
+        <div className="title-wrapper">
+          <div className="title">
+            <h1>Інклюзивний догляд за дітьми з особливими потребами</h1>
+            <div className="title-buttons">
+              <a className="sec-btn" href="/what-we-do">Що ми робимо</a>
+              <div>
+                <svg className="playicon">
+                  <use xlinkHref="/img/sprite.svg#Play icon"></use>
+                </svg>
+                <a className="video-btn" href="https://www.youtube.com/watch?v=rkRyetRMrjQ">Переглянути відео</a>
+              </div>
             </div>
           </div>
         </div>
